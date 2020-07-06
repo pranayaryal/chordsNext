1eac75fae5a1f18ced621e89d41fd04518ecfcad
Pranay Aryal <drpranayaryal@gmail.com>
2020-07-06 16:11:09 -0400

    react ga

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import Nav from '../components/nav'
 import MenuAndDetails from '../components/MenuAndDetails';
 import { getChordData } from '../lib/chords';
 import Menu from '../components/Menu';
+import ReactGA from 'react-ga'
 
 
 export default function IndexPage() {
+
+  ReactGA.initialize('UA-58522655-2')
+  ReactGA.pageview(window.location.pathname + window.location.search)
 
   const defaultData = getChordData('A');
   return (
