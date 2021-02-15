608139212dad4ced41728d3c7d63849a31e9399c
pranay <drpranayaryal@gmail.com>
2021-02-15 00:11:07 -0500

    remove plausible

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,6 @@ const Layout = ({children}) => {
   return (
     <Head>
       <title>Ukulele Chords - Pranay Aryal</title>
-      <script async defer data-domain="uku-chords.netlify.app" src="https://plausible.io/js/plausible.js"></script>
       { children }
     </Head>
 
